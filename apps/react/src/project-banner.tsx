f477b2393f276d00e69e9e764fd1dd2de60f0253
rienst <rien@stenekes.net>
2025-04-04 12:21:30 +0200

    update project banner styles

--- apps/react/src/project-banner.tsx
+++ apps/react/src/project-banner.tsx
@@ -1,35 +1,23 @@
 export function ProjectBanner() {
   return (
-    <div className="bg-neutral-100 border-b border-neutral-300 dark:text-white dark:border-neutral-700 py-2 px-4 text-sm dark:bg-neutral-800 flex items-center gap-4">
-      <h1 className="font-semibold">
-        <a
-          className="hover:underline underline-offset-2"
-          href="https://github.com/rienst/calendar-ui"
-          target="_blank"
-        >
-          rienst/calendar-ui
-        </a>
-      </h1>
-
-      <p>
-        You're viewing the React version, check out the{' '}
-        <a
-          href="https://rienst.github.io/calendar-ui/angular/"
-          target="_blank"
-          className="text-blue-600 underline underline-offset-2 dark:text-blue-400"
-        >
-          Angular version
-        </a>{' '}
-        or the{' '}
-        <a
-          href="https://github.com/rienst/calendar-ui"
-          target="_blank"
-          className="text-blue-600 underline underline-offset-2 dark:text-blue-400"
-        >
-          source on GitHub
-        </a>
-        .
-      </p>
-    </div>
+    <p className="bg-[#bbe2f0] dark:bg-[#004760] dark:text-white py-2 px-4 text-center text-sm">
+      This is a calendar UI built using React. Check out the{' '}
+      <a
+        href="https://rienst.github.io/calendar-ui/angular/"
+        target="_blank"
+        className="underline underline-offset-2"
+      >
+        Angular version
+      </a>{' '}
+      or the{' '}
+      <a
+        href="https://github.com/rienst/calendar-ui"
+        target="_blank"
+        className="underline underline-offset-2"
+      >
+        source on GitHub
+      </a>
+      .
+    </p>
   )
 }
